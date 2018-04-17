ac52e3e4abddf5bc9ab3e5929345c635f85b3b40
Jimmy Xu <xjimmyshcn@gmail.com>
2018-04-17 18:33:10 +0800

    [master] update hyper-api in vendor

--- vendor/github.com/hyperhq/hyper-api/client/pod_exec.go
+++ vendor/github.com/hyperhq/hyper-api/client/pod_exec.go
@@ -1,16 +1,20 @@
 package client
 
 import (
-	"encoding/json"
-
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/url"
+
 	"github.com/hyperhq/hyper-api/types"
 )
 
-// ContainerExecCreate creates a new exec configuration to run an exec process.
-func (cli *Client) ContainerExecCreate(ctx context.Context, container string, config types.ExecConfig) (types.ContainerExecCreateResponse, error) {
-	var response types.ContainerExecCreateResponse
-	resp, err := cli.post(ctx, "/containers/"+container+"/exec", nil, config, nil)
+// PodExecCreate creates a new exec configuration to run an exec process.
+func (cli *Client) PodExecCreate(ctx context.Context, pod, container string, config types.ExecConfig) (types.PodExecCreateResponse, error) {
+	var response types.PodExecCreateResponse
+	query := url.Values{}
+	query["container"] = []string{container}
+	resp, err := cli.post(ctx, fmt.Sprintf("/namespaces/default/pods/%v/exec", pod), query, config, nil)
 	if err != nil {
 		return response, err
 	}
@@ -19,26 +23,26 @@ func (cli *Client) ContainerExecCreate(ctx context.Context, container string, co
 	return response, err
 }
 
-// ContainerExecStart starts an exec process already created in the docker host.
-func (cli *Client) ContainerExecStart(ctx context.Context, execID string, config types.ExecStartCheck) error {
-	resp, err := cli.post(ctx, "/exec/"+execID+"/start", nil, config, nil)
+// PodExecStart starts an exec process already created in the docker host.
+func (cli *Client) PodExecStart(ctx context.Context, execID string, config types.ExecStartCheck) error {
+	resp, err := cli.post(ctx, fmt.Sprintf("/exec/%s/start", execID), nil, config, nil)
 	ensureReaderClosed(resp)
 	return err
 }
 
-// ContainerExecAttach attaches a connection to an exec process in the server.
+// PodExecAttach attaches a connection to an exec process in the server.
 // It returns a types.HijackedConnection with the hijacked connection
 // and the a reader to get output. It's up to the called to close
 // the hijacked connection by calling types.HijackedResponse.Close.
-func (cli *Client) ContainerExecAttach(ctx context.Context, execID string, config types.ExecConfig) (types.HijackedResponse, error) {
+func (cli *Client) PodExecAttach(ctx context.Context, execID string, config types.ExecConfig) (types.HijackedResponse, error) {
 	headers := map[string][]string{"Content-Type": {"application/json"}}
-	return cli.postHijacked(ctx, "/exec/"+execID+"/start", nil, config, headers)
+	return cli.postHijacked(ctx, fmt.Sprintf("/exec/%s/start", execID), nil, config, headers)
 }
 
-// ContainerExecInspect returns information about a specific exec process on the docker host.
-func (cli *Client) ContainerExecInspect(ctx context.Context, execID string) (types.ContainerExecInspect, error) {
-	var response types.ContainerExecInspect
-	resp, err := cli.get(ctx, "/exec/"+execID+"/json", nil, nil)
+// PodExecInspect returns information about a specific exec process on the docker host.
+func (cli *Client) PodExecInspect(ctx context.Context, execID string) (types.PodExecInspect, error) {
+	var response types.PodExecInspect
+	resp, err := cli.get(ctx, fmt.Sprintf("/exec/%s/json", execID), nil, nil)
 	if err != nil {
 		return response, err
 	}
