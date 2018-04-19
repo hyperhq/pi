0b33946ef487c1a72a1653e6cc0afb078232168e
bin liu <liubin0329@gmail.com>
2018-04-19 17:04:47 +0800

    Add  sub command

--- pkg/pi/cmd/config/config.go
+++ pkg/pi/cmd/config/config.go
@@ -68,7 +68,7 @@ func NewCmdConfig(pathOptions *clientcmd.PathOptions, out, errOut io.Writer) *co
 	//cmd.AddCommand(NewCmdConfigSet(out, pathOptions))
 	//cmd.AddCommand(NewCmdConfigUnset(out, pathOptions))
 	cmd.AddCommand(NewCmdConfigCurrentContext(out, pathOptions))
-	//cmd.AddCommand(NewCmdConfigUseContext(out, pathOptions))
+	cmd.AddCommand(NewCmdConfigUseContext(out, pathOptions))
 	cmd.AddCommand(NewCmdConfigGetContexts(out, pathOptions))
 	//cmd.AddCommand(NewCmdConfigGetClusters(out, pathOptions))
 	//cmd.AddCommand(NewCmdConfigDeleteCluster(out, pathOptions))
